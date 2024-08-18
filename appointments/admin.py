0495811eb85c85da036bf5bcd7098e6bca75c8d8
Leocazarini <leonardo.cazarini19@gmail.com>
2024-08-18 12:54:15 -0300

    correção das importações do módulo admin

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Infirmary, StudentAppointment, EmployeeAppointment, VisitorAppointment
+from .models import *
 
 
 # Register your models here.
 admin.site.register(Infirmary)
+admin.site.register(Nurse)
 admin.site.register(StudentAppointment)
 admin.site.register(EmployeeAppointment)
 admin.site.register(VisitorAppointment)
